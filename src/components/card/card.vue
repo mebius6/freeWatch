ec0ef3e3ac604473ae43983dee9aeb58e5c8250f
599422271@qq.com <599422271@qq.com>
2019-12-14 09:59:25 +0800

    修改布局

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,110 +1,124 @@
 <template>
-	<view class="free-watch-card">
-		<view class="card-left" v-if="showImg">
-			<view v-if="image.path">
-				<img :src="image.path" />
-			</view>
-		</view>
-		<view class="card-right">
-			<view v-for="(item, index) in columns" :key="index" class="free-watch-card-right">
-				<view class="card-right-item" v-if="!item.render">
-					<view v-if="(item.list || []).length">
-						<text v-for="(v, i) in item.list || []" :key="i">{{ v }}<text v-if="v" class="dot"></text></text>
-					</view>
-				</view>
-				<view class="card-right-item" v-else>
-					<Expand :row="item.data || {}" :index="index" :render="item.render"></Expand>
-				</view>
-			</view>
-		</view>
-	</view>
+  <view class="free-watch-card">
+    <view class="card-left" v-if="showImg">
+      <view v-if="image.path">
+        <img :src="image.path" />
+      </view>
+    </view>
+    <view class="card-right">
+      <view
+        v-for="(item, index) in columns"
+        :key="index"
+        class="free-watch-card-right"
+      >
+        <view class="card-right-item" v-if="!item.render">
+          <view v-if="(item.list || []).length">
+            <text v-for="(v, i) in item.list || []" :key="i"
+              >{{ v }}<text v-if="v" class="dot"></text
+            ></text>
+          </view>
+        </view>
+        <view class="card-right-item" v-else>
+          <Expand
+            :row="item.data || {}"
+            :index="index"
+            :render="item.render"
+          ></Expand>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	import Expand from './expand';
+import Expand from './expand';
 
-	export default {
-		name: 'graphic-text',
-		components: {
-			Expand
-		},
-		props: {
-			showImg: {
-				type: Boolean,
-				default: () => {
-					return true
-				}
-			},
-			image: {
-				type: Object,
-				default: () => {
-					return {
-						path: ''
-					};
-				}
-			},
-			columns: {
-				type: Array,
-				default: () => {
-					return [];
-				}
-			}
-		},
-		data() {
-			return {};
-		}
-	};
+export default {
+  name: 'graphic-text',
+  components: {
+    Expand
+  },
+  props: {
+    showImg: {
+      type: Boolean,
+      default: () => {
+        return true;
+      }
+    },
+    image: {
+      type: Object,
+      default: () => {
+        return {
+          path: ''
+        };
+      }
+    },
+    columns: {
+      type: Array,
+      default: () => {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="less">
-	@import '../../styles/main';
+@import '../../styles/main';
 
-	.free-watch-card {
-		margin: 10px 0;
-		width: 100%;
-		display: flex;
-		flex-direction: row;
-		padding: 0 10px;
+.free-watch-card {
+  margin: 10px 0;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 0 10px;
 
-		.card-left {
-			width: 25vw;
-			height: 25vw;
-			border: 1px solid #dbdbdb;
-			margin-right: 10px;
-			display: flex;
-			justify-content: center;
-			align-items: center;
+  .card-left {
+    width: 25vw;
+    height: 25vw;
+    max-width: 125px;
+    max-height: 125px;
+    border: 1px solid #dbdbdb;
+    margin-right: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-			img {
-				max-width: 25vw;
-				max-height: 25vw;
-			}
-		}
+    img {
+      width: 25vw;
+      height: 25vw;
+      max-width: 125px;
+      max-height: 125px;
+    }
+  }
 
-		.card-right {
-			flex: 1;
-			min-width: 0;
-			height: 100%;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			font-size: 14px;
-			color: @free-watch-from-gray-color;
+  .card-right {
+    flex: 1;
+    min-width: 0;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: 14px;
+    color: @free-watch-from-gray-color;
 
-			&-item {
-				text-align: left;
-				width: 100%;
-				.textHide();
+    &-item {
+      text-align: left;
+      width: 100%;
+      .textHide();
 
-				&>view {
-					width: 100%;
-					.textHide();
+      & > view {
+        width: 100%;
+        .textHide();
 
-					.dot {
-						margin-right: 5px;
-					}
-				}
-			}
-		}
-	}
+        .dot {
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/pages/player-item/playerItem.vue
+++ src/pages/player-item/playerItem.vue
@@ -1,181 +1,218 @@
 <template>
-	<view class="free-watch-player">
-		<!-- #ifndef H5 -->
-		<video name="media" muted="muted" autoplay="autoplay" controls="controls" loop="loop" class="free-watch-player-row" :src="url" direction="90" @fullscreenchange="fullscreenchange">
-			<source :src="url" type="video/m3u8" />
-		</video>
-		<!-- #endif-->
+  <view class="free-watch-player">
+    <!-- #ifndef H5 -->
+    <video
+      name="media"
+      muted="muted"
+      autoplay="autoplay"
+      controls="controls"
+      loop="loop"
+      class="free-watch-player-row"
+      :src="url"
+      direction="90"
+      @fullscreenchange="fullscreenchange"
+    >
+      <source :src="url" type="video/m3u8" />
+    </video>
+    <!-- #endif-->
 
-		<!-- #ifdef H5 -->
-		<dplayer v-if="video.url" class="free-watch-player-row" ref="player" @on-ended="ended" @on-playing="playing" :video="video" :contextmenu="contextmenu"></dplayer>
-		<!-- #endif -->
-		<view class="tarbar-detail-body-section-item">
-			<view :class="title===v.title?'player-title active':'player-title'" v-for="(v, i) in body" :key="i" @click="playerItem(v)">
-				<text class="player-title-text">{{ v.title }}</text>
-			</view>
-		</view>
-	</view>
+    <!-- #ifdef H5 -->
+    <dplayer
+      v-if="video.url && video.url.endsWith('.m3u8')"
+      class="free-watch-player-row"
+      ref="player"
+      @on-ended="ended"
+      @on-playing="playing"
+      :video="video"
+      :contextmenu="contextmenu"
+    ></dplayer>
+    <video
+      v-if="video.url && !video.url.endsWith('.m3u8')"
+      name="media"
+      muted="muted"
+      autoplay="autoplay"
+      controls="controls"
+      loop="loop"
+      class="free-watch-player-row"
+      :src="video.url"
+      direction="90"
+      @fullscreenchange="fullscreenchange"
+    >
+      <source :src="url" type="video/m3u8" />
+    </video>
+    <!-- #endif -->
+    <view class="tarbar-detail-body-section-item">
+      <view
+        :class="title === v.title ? 'player-title active' : 'player-title'"
+        v-for="(v, i) in body"
+        :key="i"
+        @click="playerItem(v)"
+      >
+        <text class="player-title-text">{{ v.title }}</text>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
 // #ifdef H5
-import dplayer from '../components/dplayer/dplayer'
+import dplayer from '../components/dplayer/dplayer';
 // #endif
 export default {
-	// #ifdef H5
-	components: {
-		dplayer
-	},
-	// #endif
-	data() {
-		return {
-			// #ifdef H5
-			video: {
-				url: '',
-				defaultQuality: 1,
-				type: 'hls'
-			},
-			autoplay: true,
-			player: null,
-			body: [],
-			contextmenu: [],
-			// #endif
-			option: {},
-			title: '',
-			url: '',
-			row: true // 竖屏
-		}
-	},
-	async onLoad(option) {
-		console.log(['uni', uni])
-		uni.showLoading({
-			title: this.$config.loadingText
-		})
+  // #ifdef H5
+  components: {
+    dplayer
+  },
+  // #endif
+  data() {
+    return {
+      // #ifdef H5
+      video: {
+        url: '',
+        defaultQuality: 1,
+        type: 'auto'
+      },
+      autoplay: true,
+      player: null,
+      body: [],
+      contextmenu: [],
+      // #endif
+      option: {},
+      title: '',
+      url: '',
+      row: true // 竖屏
+    };
+  },
+  async onLoad(option) {
+    console.log(['uni', uni]);
+    uni.showLoading({
+      title: this.$config.loadingText
+    });
 
-		try {
-			console.log(['option', option])
-			this.option = JSON.parse(option.data)
-			this.title = this.option.title
-			await this.getPlayerUrl(this.option)
-			//option为object类型，会序列化上个页面传递的参数
+    try {
+      console.log(['option', option]);
+      this.option = JSON.parse(option.data);
+      this.title = this.option.title;
+      await this.getPlayerUrl(this.option);
+      //option为object类型，会序列化上个页面传递的参数
 
-			this.body = JSON.parse(option.list)
-			console.log(['body', this.body]) //打印出上个页面传递的参数。
-			uni.setNavigationBarTitle({
-				title: this.option.author
-			})
-		} catch (error) {
-			console.log(error)
-		}
+      this.body = JSON.parse(option.list);
+      console.log(['body', this.body]); //打印出上个页面传递的参数。
+      uni.setNavigationBarTitle({
+        title: this.option.author
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
-		uni.hideLoading()
-	},
+    uni.hideLoading();
+  },
 
-	methods: {
-		async getPlayerUrl(item) {
-			let res = await this.api.get245BtPlayerUrl(item.path).catch(err => {
-				console.log(err)
-			})
-			if (!res) return false
-			console.log(['res', res])
-			// #ifndef H5
-			this.url = res.url
-			// #endif
+  methods: {
+    async getPlayerUrl(item) {
+      let res = await this.api.get245BtPlayerUrl(item.path).catch(err => {
+        console.log(err);
+      });
+      if (!res) return false;
+      console.log(['res', res]);
+      // #ifndef H5
+      this.url = res.url;
+      // #endif
 
-			//  #ifdef H5
-			this.video.url = res.url
-			// #endif
-		},
-		fullscreenchange(event) {
-			// 横屏
-			if (event.detail.fullScreen) {
-				this.row = false
-			} else {
-				this.row = true
-			}
-			console.log(event)
-		},
-		//播放结束
-		async ended() {
-			let vm = this
-			let index = vm.body.findIndex(v => v.title === vm.title)
-			if (index < vm.body.length - 1) {
-				console.log(['end'])
-				uni.showLoading({
-					title: this.$config.loadingText
-				})
-				let item = vm.body[index + 1]
-				vm.title = item.title
-				await this.getPlayerUrl(item)
-				uni.hideLoading()
-			}
-		},
-		//正在播放
-		async playing(data) {
-			if (data.duration - data.currentTime < 180) {
-				let vm = this
-				let index = vm.body.findIndex(v => v.title === vm.title)
-				if (index < vm.body.length - 1) {
-					console.log(['end'])
-					uni.showLoading({
-						title: this.$config.loadingText
-					})
-					let item = vm.body[index + 1]
-					vm.title = item.title
-					await this.getPlayerUrl(item)
-					uni.hideLoading()
-				}
-			}
-		},
-		async playerItem(item) {
-			uni.showLoading({
-				title: this.$config.loadingText
-			})
-			this.title = item.title
-			await this.getPlayerUrl(item)
-			uni.hideLoading()
-		}
-	}
-}
+      //  #ifdef H5
+      this.video.url = res.url;
+      // #endif
+    },
+    fullscreenchange(event) {
+      // 横屏
+      if (event.detail.fullScreen) {
+        this.row = false;
+      } else {
+        this.row = true;
+      }
+      console.log(event);
+    },
+    //播放结束
+    async ended() {
+      let vm = this;
+      let index = vm.body.findIndex(v => v.title === vm.title);
+      if (index < vm.body.length - 1) {
+        console.log(['end']);
+        uni.showLoading({
+          title: this.$config.loadingText
+        });
+        let item = vm.body[index + 1];
+        vm.title = item.title;
+        await this.getPlayerUrl(item);
+        uni.hideLoading();
+      }
+    },
+    //正在播放
+    async playing(data) {
+      if (data.duration - data.currentTime < 180) {
+        let vm = this;
+        let index = vm.body.findIndex(v => v.title === vm.title);
+        if (index < vm.body.length - 1) {
+          console.log(['end']);
+          uni.showLoading({
+            title: this.$config.loadingText
+          });
+          let item = vm.body[index + 1];
+          vm.title = item.title;
+          await this.getPlayerUrl(item);
+          uni.hideLoading();
+        }
+      }
+    },
+    async playerItem(item) {
+      uni.showLoading({
+        title: this.$config.loadingText
+      });
+      this.title = item.title;
+      await this.getPlayerUrl(item);
+      uni.hideLoading();
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
 @import '../../styles/main.less';
 .free-watch-player {
-	&-row {
-		width: 100vw;
-		height: 40vh;
-		transform: rotate(0);
-	}
-	&-column {
-		width: 100vh;
-		height: 100vw;
-		transform: rotate(90);
-	}
-	.vjs-big-play-button {
-		left: 50% !important;
-		top: 50% !important;
-	}
+  &-row {
+    width: 100vw;
+    height: 50vh;
+    transform: rotate(0);
+  }
+  &-column {
+    width: 100vh;
+    height: 100vw;
+    transform: rotate(90);
+  }
+  .vjs-big-play-button {
+    left: 50% !important;
+    top: 50% !important;
+  }
 }
 .tarbar-detail-body-section-item {
-	font-size: 14px;
-	display: flex;
-	justify-content: flex-start;
-	padding: 20px 10px;
-	flex-wrap: wrap;
-	.player-title {
-		width: 17vw;
-		text-align: center;
-		color: #333;
-		border: 1px solid #eee;
-		border-radius: 2px;
-		padding: 2px 5px;
-		margin: 2px;
-		&.active {
-			font-size: 16px;
-			color: @free-watch-main-color;
-		}
-		.textHide();
-	}
+  font-size: 14px;
+  display: flex;
+  justify-content: flex-start;
+  padding: 20px 10px;
+  flex-wrap: wrap;
+  .player-title {
+    width: 17vw;
+    text-align: center;
+    color: #333;
+    border: 1px solid #eee;
+    border-radius: 2px;
+    padding: 2px 5px;
+    margin: 2px;
+    &.active {
+      font-size: 16px;
+      color: @free-watch-main-color;
+    }
+    .textHide();
+  }
 }
 </style>
